d5e9c17b4583c7423cf0654ebf2948aec88626bb
TR0D30T <44397261+TR0D30T@users.noreply.github.com>
2021-07-28 22:14:00 +0200

    Update app.js

--- Tech-Degree-Project-8/js/app.js
+++ Tech-Degree-Project-8/js/app.js
@@ -1,29 +1,36 @@
+//********************************** */
 //Variables
+//********************************** */
 let employees = [];
 const apiURL = 'https://randomuser.me/api/?results=12&inc=name, picture, email, location, phone, dob &noinfo &nat=US';
 const gridContainer = document.querySelector(".grid-container");
 const overlay = document.querySelector(".overlay");
 const modalContainer = document.querySelector(".modal-content");
 const modalClose = document.querySelector(".modal-close");
-const searchBar = document.querySelector(".search-bar");
+const searchBar = document.querySelector('.search-bar');
 const arrowRight = document.querySelector(".arrow-right");
 const arrowLeft = document.querySelector(".arrow-left");
+const cards = document.querySelectorAll(".card");
 let modalIndex;
 
 
+
+
+//********************************** */
 //Fetch
+//********************************** */
 fetch(apiURL)
     .then(res => res.json())
     .then(data => data.results)
     //.then(data => console.log(data))
     .then(displayEmployees)
-    
     .catch(error => console.log("there was a problem", error))
 
 
-//---------
+
+//********************************** */
 //Helper functions
-//---------
+//********************************** */
 function displayEmployees(employeeData) {
     employees = employeeData;
     //Store HTML for employee
@@ -37,7 +44,7 @@ function displayEmployees(employeeData) {
         let country = employee.location.country;
         let picture = employee.picture;
     
-    //template literal
+    //template literal -> build HTML
     employeeHTML += `
         <div class="card" data-index="${index}">
             <img class="avatar" src="${picture.large}"/>
@@ -51,7 +58,6 @@ function displayEmployees(employeeData) {
     `
     });
     gridContainer.innerHTML = employeeHTML;
-    
 }
 
 
@@ -75,15 +81,16 @@ function displayModal(index) {
                 ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
                 </div>
         `
-        
         overlay.classList.remove("hidden");
         modalContainer.innerHTML = modalHTML;
         modalIndex = index;
+        //Every time the modal window is opened, it will change the value of the modalIndex variable 
+        //that you already created to be the index of whatever modal window is currently open. 
 }
 
-//======
+//********************************** */
 // Event Listeners 
-//=====
+//********************************** */
 gridContainer.addEventListener('click', e => {
     //
     if (e.target !== gridContainer) {
@@ -94,18 +101,20 @@ gridContainer.addEventListener('click', e => {
         displayModal(index);
     }
 
-})
-
-//Modal close event
-modalClose.addEventListener('click', (e) => {
-    
-    overlay.classList.add("hidden");
-    
 });
 
-//********** 
+
+//********************************** */
+//Modal close event
+//********************************** */
+modalClose.addEventListener('click', (e) => {
+    overlay.classList.add("hidden");
+});
+
+
+//********************************** */
 //Arrow modal
-//********** 
+//********************************** */
 arrowRight.addEventListener('click', e =>  {  
     if(modalIndex < 11){
         modalIndex++;
@@ -129,31 +138,34 @@ arrowLeft.addEventListener('click', e =>  {
 });
 
 
-//** Filter search feature on 'keyup' 
-
-const cardNames = document.querySelectorAll('.text-container h2');
-//console.log(cardNames); --> NodeList[]
+//********************************** */
+//** Filter search feature on 'keyup'
+//********************************** */
 
 const handleSearch = e => {
+    //create cardNames inside handler, to make sure that the fetch can provide
+    //data when the page is loaded
+    //if cardNames is create before thehandler, there is no data to get for the loop
+    const cardNames = document.querySelectorAll('.text-container h2');
     const searchTarget = e.target.value.toLowerCase();
 
     cardNames.forEach(cardName => {
         const name = cardName.textContent.toLowerCase();
-        const card = cardName.parentElement;
-        
+
+        //target element (cardName).parentElement.parentElement to get to
+        // .card class (as parent to work with it)
+        //.parentElement -> .text-container
+        //.parentElement -> .card
+        const card = cardName.parentElement.parentElement;
+        //console.log(card);
 
         if(name.includes(searchTarget)) {
             card.style.display = "flex";
         } else {
-            card.style.display = "";
+            card.style.display = "none";
         }
-        
     });
-   
-
-
 }
-
 searchBar.addEventListener('keyup', handleSearch);
 
 
